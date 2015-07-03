188b7571504c805301a099077ecf2cbb3b1da28d
Markr10 <mark-r@live.nl>
2015-07-03 17:04:06 +0200

    Resize with existing thumbnail If the image does not contain a thumbnail, this method creates a thumbnail image by scaling the main image.

---
+++ ImagesToThumbnails/ResizeImage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagesToThumbnails
+{
+    public partial class ResizeImage : Form
+    {
+        public ResizeImage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string filename = openFileDialog1.FileName;
+            using (var image = Image.FromFile(filename))
+            {
+                using (var thumbnail = image.GetThumbnailImage(image.Width / 10, image.Height / 10, null, IntPtr.Zero))
+                {
+                    thumbnail.Save(openFileDialog1.FileName + "thumb.png");
+                }
+            }
+        }
+    }
+}
