60d9c1ab8d9144174130e24060c8d3e1fa7e25ec
Markr10 <mark-r@live.nl>
2015-07-14 21:50:51 +0200

    Fit mode added

---
+++ ImagesToThumbnails/FitMode.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ImagesToThumbnails
+{
+    public enum FitMode
+    {
+        Fit,
+        FitWidth,
+        FitHeight,
+        Stretch,
+    }
+}
+
+// Self defined namespace for classes with extension method(s).
+namespace Extension
+{
+    using ImagesToThumbnails;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Class that contains extension method(s) for the FitMode enum.
+    /// </summary>
+    /// <remarks>The extension method(s) can only be accessed when the Extension namespace is imported.</remarks>
+    public static class FitModeExtensionMethods
+    {
+        public static string ToSentenceCase(this FitMode fitMode)
+        {
+            Regex rgx = new Regex(@"
+                (?<=[A-Z])(?=[A-Z][a-z])(.) |
+                 (?<=[^A-Z])(?=[A-Z][A-Z]) |
+                 (?<=[^A-Z])(?=[A-Z])(.)", RegexOptions.IgnorePatternWhitespace);
+
+            return rgx.Replace(fitMode.ToString(), s => " " + s.Value.ToLower());
+        }
+    }
+}
